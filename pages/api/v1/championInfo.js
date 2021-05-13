9ff90f24b2941f116f4f09b22fdbe8d2f5c40a88
herbertma <hma49@sfu.ca>
2021-05-13 16:04:30 -0700

    added some more comments

--- pages/api/v1/championInfo.js
+++ pages/api/v1/championInfo.js
@@ -3,12 +3,14 @@ import cache from "../../../middleware/cache"
 
 const handler = async (req, res) => {
 
+   // Check the cache before making a call
   const cachedInfo = cache.get("championInfo");
   if(cachedInfo){
    res.send(cachedInfo);
    return;
   }
 
+    // Cached response is no longer available, make a fetch request
   const championInfo = await getChampionInfo();
 
   if(championInfo.ERROR){
